bc4f10d7c53d01d11b33c8c2b075400c6833a5b7
larooo <larahosni84@hotmail.com>
2019-10-08 13:45:18 +0200

    Updates

---
+++ precache-manifest.d6fdd383b96c08f9fd6f55952e85a491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3651e0595fb9045c0c0611764b2a021b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfca1362e58efaf8b679",
+    "url": "/static/css/main.2fd665b9.chunk.css"
+  },
+  {
+    "revision": "c5bd9b71e5bdcb0a564e",
+    "url": "/static/js/2.95580638.chunk.js"
+  },
+  {
+    "revision": "bfca1362e58efaf8b679",
+    "url": "/static/js/main.5e7d90f3.chunk.js"
+  },
+  {
+    "revision": "7fcf3e3f4c122ddb08d3",
+    "url": "/static/js/runtime-main.3da14bdd.js"
+  }
+]);
